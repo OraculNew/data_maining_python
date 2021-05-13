9887343ee908fd1b7575411add8bf23b8ed3f05e
OraculNew <72414396+OraculNew@users.noreply.github.com>
2021-05-14 00:35:47 +0300

    first

---
+++ gb_parse/spiders/avito.py
@@ -0,0 +1,43 @@
+import scrapy
+from ..loaders import VacancyLoader, CompanyLoader
+from ..items import VacancyItem, CompanyItem
+from .xpaths import SELECTORS, DATA_VACANCY_SELECTORS, DATA_COMPANY_SELECTORS
+
+class AvitoSpider(scrapy.Spider):
+    name = 'avito'
+    allowed_domains = ['avito.ru']
+    start_urls = ['https://www.avito.ru/domodedovo/kvartiry/prodam']
+
+    def _get_follow(self, response, selector_str, callback):
+        for itm in response.xpath(selector_str):
+            yield response.follow(itm, callback=callback)
+
+    def parse(self, response, *args, **kwargs):
+        yield from self._get_follow(
+            response, SELECTORS["vacancy_pagination"], self.parse
+        )
+        yield from self._get_follow(
+            response, SELECTORS["vacancy_url"], self.vacancy_parse
+        )
+
+    def vacancy_parse(self, response):
+        item = VacancyItem()
+        vacancy_loader = VacancyLoader(response=response, item=item)
+        vacancy_loader.add_value("url", response.url)
+        for key, xpath in DATA_VACANCY_SELECTORS.items():
+            vacancy_loader.add_xpath(key, xpath)
+        yield from self._get_follow(
+            response, SELECTORS["company_url"], self.company_parse
+        )
+        yield vacancy_loader.load_item()
+
+    def company_parse(self, response):
+        item = CompanyItem()
+        company_loader = CompanyLoader(response=response, item=item)
+        company_loader.add_value("url", response.url)
+        for key, xpath in DATA_COMPANY_SELECTORS.items():
+            company_loader.add_xpath(key, xpath)
+        yield from self._get_follow(
+            response, SELECTORS["company_vacancies"], self.parse
+        )
+        yield company_loader.load_item()
